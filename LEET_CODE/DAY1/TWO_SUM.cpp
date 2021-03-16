ad71ab8a5ac616d4d962327c65adf71d956c2286
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2021-03-16 17:04:39 +0530

    Solution using map in c++

--- LEET_CODE/DAY1/TWO_SUM.cpp
+++ LEET_CODE/DAY1/TWO_SUM.cpp
@@ -1,1 +1,16 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> nmap;
+        vector<int> v;
+        for(int i=0;i<nums.size();i++){
+            if(nmap.find(target-nums[i])!=nmap.end()){
+                v.push_back(nmap[target-nums[i]]);
+                v.push_back(i);
+                return v;
+            }
+            nmap.insert({nums[i],i});
+        }
+        return v;
+    }
+};
