82c257029c9b79c82e45ffdd6fb351612304560d
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2021-03-16 17:30:44 +0530

    Solution Using for loop and if else

--- LEET_CODE/DAY1/BEST_TIME_TO_BUY_AND_SELL_STOCK.cpp
+++ LEET_CODE/DAY1/BEST_TIME_TO_BUY_AND_SELL_STOCK.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_num=prices[0];
+        int max_profit=0;
+        for(int i=0;i<prices.size();i++){
+            if(min_num>prices[i]){
+                min_num=prices[i];
+            }
+            else
+            if(abs(min_num-prices[i])>max_profit){
+                max_profit=abs(min_num-prices[i]);
+            }
+        }
+        return max_profit;
+    }
+};
