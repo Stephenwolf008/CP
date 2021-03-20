ff90cf61fef4846d19ef4621052fa10108185087
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2021-03-21 00:16:57 +0530

    Solution which is 100% faster than other c++ ones

--- LEET_CODE/DAY2/VALID_PARANTHESIS.cpp
+++ LEET_CODE/DAY2/VALID_PARANTHESIS.cpp
@@ -1,1 +1,32 @@
-
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.length()%2!=0)
+            return false;
+        vector<char> stack;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(')
+                stack.push_back(s[i]);
+            else if(s[i]=='{')
+                stack.push_back(s[i]);
+            else if(s[i]=='[')
+                stack.push_back(s[i]);
+            else if(stack.size()!=0 && stack.back()=='(' ){
+                if(s[i]==')')
+                stack.pop_back();
+                else return false;}
+            else if(stack.size()!=0 && stack.back()=='{' ){
+                if(s[i]=='}')
+                stack.pop_back();
+                else return false;}
+            else if(stack.size()!=0 && stack.back()=='[' ){
+                if(s[i]==']')
+                stack.pop_back();
+                else return false;}
+            else return false;
+        }
+        if(stack.size()==0)
+            return true;
+        else return false;
+    }
+};
