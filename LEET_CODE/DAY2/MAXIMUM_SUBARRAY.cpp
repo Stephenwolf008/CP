845873312437d1c1a48a35042bd860a3f74ee20b
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2021-03-20 19:32:27 +0530

    Solution Using simple approach

--- LEET_CODE/DAY2/MAXIMUM_SUBARRAY.cpp
+++ LEET_CODE/DAY2/MAXIMUM_SUBARRAY.cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int num=*max_element(nums.begin(),nums.end());
+        if(num<0)
+            return num;
+        int curr=0;
+        int max=0;
+        for(int i=0;i<nums.size();i++){
+            curr += nums[i];
+            if(curr>max)
+                max=curr;
+            if(curr<0)
+                curr=0;
+        }
+        return max;
+    }
+};
