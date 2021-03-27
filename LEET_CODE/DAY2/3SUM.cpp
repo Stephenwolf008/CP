48f63ce83378656d6724d8f03ed9667e122361ae
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2021-03-27 14:07:14 +0530

    Solution using two pointer approach

---
+++ LEET_CODE/DAY2/3SUM.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> answer;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if (i != 0 && nums[i] == nums[i - 1]) continue;
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k]==0){
+                    answer.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                while(nums[j]==nums[j-1] && j<k)j++;
+            }else if(nums[i]+nums[j]+nums[k]<0)
+                j++;
+            else k--;
+            }
+        }
+        return answer;
+    }
+};
