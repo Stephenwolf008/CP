6ffe1e692ba370eb7ebdbc4bd67943c19e367e0d
Rakshit kaushik <53433960+Stephenwolf008@users.noreply.github.com>
2021-03-27 16:15:48 +0530

    Solution using sort and compare

---
+++ LEET_CODE/DAY3/MERGE_INTERVAL.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> answer;
+        vector<int> temp;
+        sort(intervals.begin(),intervals.end());
+        temp = intervals[0];
+        for(auto i:intervals){
+            vector<int> interval=i;
+            if(temp[1]<interval[0]){
+                answer.push_back(temp);
+                temp=interval;
+            }
+            else temp[1]=max(temp[1],interval[1]);
+        }
+        answer.push_back(temp);
+        return answer;
+        
+    }
+};
